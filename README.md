6629d0911335abb3ffa60830a9721d37208b4743
Hiep Dinh <hiep.dinh@okiela.com>
2018-11-17 18:59:01 +0700

    Failure

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 This is a simple app to detect current owner of phone number via Zalo.
 
 Solution log:
-1. Using Zalo API to get owner information but I cant do it, beacause Zalo never reach that API.
-2. Use https://chat.zalo.me/ with selenium-webdriver to get owner information on searchbar
+1. Using Zalo API to get owner information => I cant do it, beacause Zalo never reach that API.
+2. Use https://chat.zalo.me/ with selenium-webdriver to get owner information on searchbar => Blocked by captcha
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,35 @@
+require 'capybara'
+require 'selenium-webdriver'
+require 'chromedriver-helper'
+require 'capybara-webkit'
+require 'pry'
+require 'rubygems'
+require 'capybara/dsl'
+
 class HomeController < ApplicationController
+  include Capybara::DSL
+
   def index
-    @results = [
-      {
-          name: 'Hiep',
-          phone: '11234'
-      }, {
-        name: 'Hiep2',
-        phone: '11234'
-      }
-    ]
+    @search_form = params[:search].presence || {}
+    if @search_form.present?
+      @phones = @search_form[:phones].to_s.split(',').reject(&:blank?)
+      if @phones.any?
+         @phones.each do |phone|
+          # Capybara.current_driver = :selenium
+          # Capybara.app_host = 'http://www.google.com'
+          driver = Selenium::WebDriver.for :chrome
+          driver.navigate.to 'https://chat.zalo.me/'
+
+          phone_field = driver.find_element(name: 'phone_num')
+          phone_field.send_keys "0785286828"
+          pass_field = driver.find_element(css: "input[placeholder='Mật khẩu']")
+          pass_field.send_keys "xxxx"
+
+          submit_button = driver.find_element(link: "Đăng nhập với mật khẩu")
+          submit_button.click
+         end
+      end
+    end
+    
   end
 end
